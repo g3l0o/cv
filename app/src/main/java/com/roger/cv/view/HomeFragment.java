b2c2ccb43297813c566285ee45bc55510fe18dc7
Rogelio <rogelio.rivera.isc@gmail.com>
2020-07-26 23:30:40 -0500

    Click databinding implemented

--- app/src/main/java/com/roger/cv/view/HomeFragment.java
+++ app/src/main/java/com/roger/cv/view/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
@@ -16,67 +17,77 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.roger.cv.R;
+import com.roger.cv.databinding.FragmentHomeBinding;
+import com.roger.cv.view.home.HomeClickListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements HomeClickListener {
 
-    @BindView(R.id.card_job)
-    CardView mCardViewJob;
-
-    @BindView(R.id.card_academy)
-    CardView mCardViewAcademy;
-
-    @BindView(R.id.card_courses)
-    CardView mCardViewCourse;
-
-    @BindView(R.id.card_certifications)
-    CardView mCardViewCertifications;
-
-    @BindView(R.id.card_technical)
-    CardView mCardViewTechnical;
-
-    @BindView(R.id.card_exracurricular)
-    CardView mCardViewExtracurricular;
-
-    @BindView(R.id.card_contests)
-    CardView mCardViewContests;
-
-    @BindView(R.id.card_books)
-    CardView mCardViewBooks;
-
-    @BindView(R.id.card_facts)
-    CardView mCardViewFacts;
+    private FragmentHomeBinding binding;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mCardViewJob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onGoToDetails();
-            }
-        });
+        binding.setClickListener(this);
     }
 
-    private void onGoToDetails(){
+    @Override
+    public void onJobExperienceClicked(View v) {
         NavDirections action = HomeFragmentDirections.actionExperience();
-        Navigation.findNavController(mCardViewJob).navigate(action);
+        Navigation.findNavController(v).navigate(action);
     }
 
+    @Override
+    public void onAcademyClicked(View v) {
+
+    }
+
+    @Override
+    public void onCoursesClicked(View v) {
+
+    }
+
+    @Override
+    public void onCertificationsClicked(View v) {
+
+    }
+
+    @Override
+    public void onTechnicalClicked(View v) {
+
+    }
+
+    @Override
+    public void onExtracurricularClicked(View v) {
+
+    }
+
+    @Override
+    public void onContestsClicked(View v) {
+
+    }
+
+    @Override
+    public void onBooksClicked(View v) {
+
+    }
+
+    @Override
+    public void onFactsClicked(View v) {
+
+    }
 }
