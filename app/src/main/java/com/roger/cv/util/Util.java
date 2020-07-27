75d1e852ce719b796b0b37309f6faf8d50df294a
Rogelio <rogelio.rivera.isc@gmail.com>
2020-07-26 22:57:19 -0500

    DataBinding in Detail Job Fragment

--- app/src/main/java/com/roger/cv/util/Util.java
+++ app/src/main/java/com/roger/cv/util/Util.java
@@ -9,21 +9,24 @@ import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.roger.cv.R;
 
 public class Util {
 
     @BindingAdapter("android:imageUrl")
     public static void loadImage(ImageView imageView, String imageName){
+        if(imageName != null) {
+            CircularProgressDrawable circularProgressDrawable = getCircularProgressDrawable(imageView.getContext());
 
-        CircularProgressDrawable circularProgressDrawable = getCircularProgressDrawable(imageView.getContext());
+            RequestOptions options = new RequestOptions()
+                    .placeholder(circularProgressDrawable)
+                    .error(R.drawable.ic_work);
 
-        RequestOptions options = new RequestOptions()
-                .placeholder(circularProgressDrawable);
-
-        GlideApp.with(imageView.getContext())
-                .setDefaultRequestOptions(options)
-                .load(getFirebaseImageLocation(imageName))
-                .into(imageView);
+            GlideApp.with(imageView.getContext())
+                    .setDefaultRequestOptions(options)
+                    .load(getFirebaseImageLocation(imageName))
+                    .into(imageView);
+        }
     }
 
     private static StorageReference getFirebaseImageLocation(String imageUrl){
--- app/src/main/java/com/roger/cv/view/job/JobDetailFragment.java
+++ app/src/main/java/com/roger/cv/view/job/JobDetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.roger.cv.R;
+import com.roger.cv.databinding.FragmentJobDetailBinding;
 import com.roger.cv.model.Job;
 import com.roger.cv.util.Util;
 import com.roger.cv.viewmodel.JobDetailViewModel;
@@ -25,15 +27,7 @@ import butterknife.ButterKnife;
 public class JobDetailFragment extends Fragment {
 
     private JobDetailViewModel viewModel;
-
-    @BindView(R.id.image_job_logo)
-    ImageView mImageViewJobLogo;
-
-    @BindView(R.id.text_company_name)
-    TextView mJobName;
-
-    @BindView(R.id.text_job_position)
-    TextView mJobPosition;
+    private FragmentJobDetailBinding binding;
 
     public JobDetailFragment() {
         // Required empty public constructor
@@ -43,9 +37,8 @@ public class JobDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_job_detail, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_job_detail, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -68,9 +61,7 @@ public class JobDetailFragment extends Fragment {
         viewModel.jobLiveData.observe(getViewLifecycleOwner(), new Observer<Job>() {
             @Override
             public void onChanged(Job job) {
-                Util.loadImage(mImageViewJobLogo, job.getLogo());
-                mJobName.setText(job.getName());
-                mJobPosition.setText(job.getJobPosition());
+               binding.setJob(job);
             }
         });
     }
