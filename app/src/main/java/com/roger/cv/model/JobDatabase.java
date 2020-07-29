7be3803ce60fbf1c89ef22e4fe46b1313195c7f3
Rogelio <rogelio.rivera.isc@gmail.com>
2020-07-28 23:47:46 -0500

    Profile fragment finished

--- app/src/main/java/com/roger/cv/model/JobDatabase.java
+++ app/src/main/java/com/roger/cv/model/JobDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Job.class}, version = 1)
+@Database(entities = {Job.class}, version = 1, exportSchema = false)
 public abstract class JobDatabase extends RoomDatabase {
 
     private static JobDatabase instance;
