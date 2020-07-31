b9f376a1af945833091b284bfcad21fbe306ff87
Rogelio <rogelio.rivera.isc@gmail.com>
2020-07-31 01:15:23 -0500

    CourseFragment implemented

--- app/src/main/java/com/roger/cv/view/course/CoursesFragment.java
+++ app/src/main/java/com/roger/cv/view/course/CoursesFragment.java
@@ -1,4 +1,4 @@
-package com.roger.cv.view.certificate;
+package com.roger.cv.view.course;
 
 import android.os.Bundle;
 
@@ -16,31 +16,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.roger.cv.R;
-import com.roger.cv.databinding.CertificateItemBinding;
 import com.roger.cv.databinding.FragmentCertificationsBinding;
+import com.roger.cv.databinding.FragmentCoursesBinding;
 import com.roger.cv.model.Certificate;
+import com.roger.cv.model.Course;
+import com.roger.cv.view.certificate.CertificateListAdapter;
 import com.roger.cv.viewmodel.CertificateViewModel;
+import com.roger.cv.viewmodel.CourseListViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class CertificatesFragment extends Fragment {
+public class CoursesFragment extends Fragment {
 
-    private CertificateViewModel viewModel;
-    private FragmentCertificationsBinding binding;
-    private CertificateListAdapter certificateListAdapter = new CertificateListAdapter(new ArrayList<Certificate>());
+    private CourseListViewModel viewModel;
+    private FragmentCoursesBinding binding;
+    private CourseListAdapter courseListAdapter = new CourseListAdapter(new ArrayList<Course>());
 
 
-    public CertificatesFragment() {
+    public CoursesFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_certifications, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_courses, container, false);
         return binding.getRoot();
     }
 
@@ -48,37 +52,36 @@ public class CertificatesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        viewModel = ViewModelProviders.of(this).get(CertificateViewModel.class);
+        viewModel = ViewModelProviders.of(this).get(CourseListViewModel.class);
         viewModel.fetchData();
 
-        binding.recyclerCertificateList.setLayoutManager(new LinearLayoutManager(getContext()));
-        binding.recyclerCertificateList.setAdapter(certificateListAdapter);
+        binding.recyclerCourseList.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerCourseList.setAdapter(courseListAdapter);
 
-        binding.swiperefreshCertificates.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        binding.swiperefreshCourse.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                binding.swiperefreshCertificates.setRefreshing(false);
+                binding.swiperefreshCourse.setRefreshing(false);
                 refreshManually();
             }
         });
 
         observeViewModel();
-
     }
 
     private void refreshManually() {
-        binding.recyclerCertificateList.setVisibility(View.GONE);
+        binding.recyclerCourseList.setVisibility(View.GONE);
         binding.textErrorMessage.setVisibility(View.GONE);
         binding.progressLoading.setVisibility(View.VISIBLE);
         viewModel.fetchData();
     }
 
     private void observeViewModel() {
-        viewModel.certificatesList.observe(getViewLifecycleOwner(), new Observer<List<Certificate>>() {
+        viewModel.courseList.observe(getViewLifecycleOwner(), new Observer<List<Course>>() {
             @Override
-            public void onChanged(List<Certificate> certificates) {
-                binding.recyclerCertificateList.setVisibility(View.VISIBLE);
-                certificateListAdapter.updateCertificateList(certificates);
+            public void onChanged(List<Course> courses) {
+                binding.recyclerCourseList.setVisibility(View.VISIBLE);
+                courseListAdapter.updateCourseList(courses);
             }
         });
 
@@ -87,16 +90,16 @@ public class CertificatesFragment extends Fragment {
             public void onChanged(Boolean isError) {
                 binding.progressLoading.setVisibility(View.GONE);
                 binding.textErrorMessage.setVisibility(isError ? View.VISIBLE : View.GONE);
-                binding.recyclerCertificateList.setVisibility(isError ? View.GONE : View.VISIBLE);
+                binding.recyclerCourseList.setVisibility(isError ? View.GONE : View.VISIBLE);
             }
         });
 
         viewModel.isLoading.observe(getViewLifecycleOwner(), new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean isLoading) {
-                if(isLoading){
+                if (isLoading) {
                     binding.textErrorMessage.setVisibility(View.GONE);
-                    binding.recyclerCertificateList.setVisibility(View.GONE);
+                    binding.recyclerCourseList.setVisibility(View.GONE);
                 }
             }
         });
